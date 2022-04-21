2b360ad8158c444c8019c80fab8469f448b05c3d
Lud <instimila@gmail.com>
2022-04-21 02:04:43 +0200

    tela 3

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@
 function criaQuizz() { 
     
 }
+function criarPerguntas(){
+   
+}
+function criarNiveis(){
+
+}
+function finalizarQuizz(){
+
+}
+function acessarQuizz(){
+
+}
+function voltarHome(){
+    
+}
