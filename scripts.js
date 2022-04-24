713fa0a8b4718bbda103030927759f5840e19820
Lud <instimila@gmail.com>
2022-04-24 21:39:47 +0200

    um pouco menos pior ainda

--- scripts.js
+++ scripts.js
@@ -42,16 +42,17 @@ function selecionarQuizz(elemento){
   let id =elemento.id
   console.log(id)
   let promise= axios.get(`https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${id}`)
-  promise.then(selecionarSucesso)
+  promise.then(selecionarSucesso )
   promise.catch(selecionarErro)
     
     
 } 
 
         function selecionarSucesso(response){   
-          
+            
             let elemento= response.data  
-            renderizarCima(elemento)  
+            renderizarCima (elemento)
+          
             console.log(elemento)    
             document.querySelector(".tela1").classList.add("escondido")
             document.querySelector(".tela2").classList.remove("escondido")
@@ -59,35 +60,36 @@ function selecionarQuizz(elemento){
         }
                 function renderizarCima(elemento){ 
                     renderizarBloco(elemento)
-                    let selecionado =document.querySelector(".tela2")
-                            selecionado.innerHTML="";               
-                            selecionado.innerHTML+=`            
-                                <div class="parteDeCima">
+                    let selecionado =document.querySelector(".parteDeCima")
+                    selecionado.innerHTML="";               
+                    selecionado.innerHTML+=`            
+                             
                                     <img src="${elemento.image}">
                                     <h2>${elemento.title}</h2>               
-                                </div>                         
-                            ` 
-                          
-                            console.log(elemento)                          
+                                                   
+                         
+                            `                                   
                 }
-               
+
                 function renderizarBloco(elemento){
-                    let block= document.querySelector(".block")
-                    let element = elemento.questions
-                    block.innerHTML="";  
-                   for(i=0;i<element.length;i++){
-                        block.innerHTML+=`      
-                        <div class="titulo-pergunta">          
-                                <h3>${element[i].title}</h3>            
-                        </div>  `    
-                                                     
-                    }
-                    console.log(block)
-                   
+                    let block =document.querySelector(".block")
+                            block.innerHTML+=`  
+                            <div class="titulo-pergunta">          
+                            <h3>${elemento.questions[0].title}<h3>            
+                        </div>      
+                        <div class="respostas">   
+                        <div class="primeira">
+                        <div class="opcao">
+                        <img src="${elemento.questions[0].answers[0].image}" />
+                        </div>
+                        <div class="nome-opcao">${elemento.questions[0].answers[0].text}</div>
+                        </div>   
+                            `
 
+                                console.log(block)
+                                
                 }
-
-            
+          
 function selecionarErro(){
     console.log("deu ruim")
 }
